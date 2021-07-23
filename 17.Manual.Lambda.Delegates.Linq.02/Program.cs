254908f05b413660b9a50664c02183037b15bccf
marcu <marcu@LAPTOP-MTO8J5A0>
2021-07-23 17:09:11 -0300

    Adicionar arquivos de projeto.

---
+++ 17.Manual.Lambda.Delegates.Linq.02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace _17.Manual.Lambda.Delegates.Linq._02 {
+    class Program {
+        public static int globalValue = 3;
+        static void Main(string[] args) {
+            int[] vect = new int[] { 3, 4, 5 };
+            ChangeOddValues(vect);
+            Console.WriteLine(string.Join(" ", vect));
+        }
+
+        public static void ChangeOddValues(int[] numbers) {
+            for(int i = 0; i < numbers.Length; i++) {
+                if(numbers[i]%2 != 0) {
+                    numbers[i] += globalValue;
+                }
+            }
+        }
+    }
+}
